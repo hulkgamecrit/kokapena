a1c7304ee842219d4fa3e60e478c9c9f7748f9fe
cdonaldson <cdonaldson@mobiquityinc.com>
2014-06-11 09:03:49 -0400

    done

---
+++ Kokapena.mm
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?><map version="0.8.1"><node CREATED="1402491755157" ID="0tmpl47kk1cpl73itrnmhvvifc" MODIFIED="1402491755157" TEXT="Kokapena"><node CREATED="1402491755157" ID="20i0ato2hamgbvsmfbkrf1a697" MODIFIED="1402491755157" POSITION="right" TEXT="API"/><node CREATED="1402491755157" ID="366cpt0jfc1lhlbh0221qqodgq" MODIFIED="1402491755157" POSITION="right" TEXT="Functional"><node CREATED="1402491755157" ID="7h5bl71s09spbrpcfn2jfnhjvm" MODIFIED="1402491755157" TEXT="Interaction"><node CREATED="1402491755158" ID="5v8mblrremdvr2rpr7o1irv7mt" MODIFIED="1402491755158" TEXT="Venue"><node CREATED="1402491755158" ID="5b2u08n3g966ct6eta0c7v1hji" MODIFIED="1402491755158" TEXT="Display Venues"><node CREATED="1402491755158" ID="5n7ma4ahvpqo1v8hv0jureslqf" MODIFIED="1402491755158" TEXT="New,Edit,Delete"><node CREATED="1402491755158" ID="0k6el0citogptlnpifpjfmbql7" MODIFIED="1402491755158" TEXT="Personal info"/><node CREATED="1402491755158" ID="3ed29moo0dqvhfai82ikrr41su" MODIFIED="1402491755158" TEXT="Photo"/></node></node><node CREATED="1402491755158" ID="0qn2g3j1d5nf7gt7ii7u49a1o8" MODIFIED="1402491755158" TEXT="TimeWall"><node CREATED="1402491755158" ID="7jbj2vnafgu8ncgphe6s4fqn7t" MODIFIED="1402491755158" TEXT="Censor"/></node><node CREATED="1402491755158" ID="4p7318khbhmp72gqpokeotja6f" MODIFIED="1402491755158" TEXT="Search Function"><node CREATED="1402491755158" ID="650p2o1434n5orm2n4lp537stt" MODIFIED="1402491755158" TEXT="Censor"/></node><node CREATED="1402491755158" ID="6hgcqassqn4uk5lorknt400uin" MODIFIED="1402491755158" TEXT="Uploading "><node CREATED="1402491755158" ID="4bn538ii4iqs800ltbjhc507f8" MODIFIED="1402491755158" TEXT="photo interaction"/></node><node CREATED="1402491755158" ID="20t36ve9hsa6gp3882qmtdknc8" MODIFIED="1402491755158" TEXT="Venue History"/><node CREATED="1402491755158" ID="41nmlfl5cmp73bj98grimu3in3" MODIFIED="1402491755158" TEXT="Offers"><node CREATED="1402491755158" ID="4p6b6hhuhi07c2p34m558dvper" MODIFIED="1402491755158" TEXT=" Listing"/><node CREATED="1402491755158" ID="44ibt6859n3u8pnb8cqpbd2pua" MODIFIED="1402491755158" TEXT="New, Edit, Delete"/><node CREATED="1402491755158" ID="0opvkl8d5pae7pm7l6lgi11cda" MODIFIED="1402491755158" TEXT="Menu Page"/></node><node CREATED="1402491755158" ID="6n1l3ij3iqev3l5gqvsqpgu3a4" MODIFIED="1402491755158" TEXT="Validation?"/><node CREATED="1402491755158" ID="246ttjbngf4nrk75bkav2875fr" MODIFIED="1402491755158" TEXT="Comments"><node CREATED="1402491755158" ID="4hhdj4k38j8mbs7lq5i6casd01" MODIFIED="1402491755158" TEXT="Censor"/><node CREATED="1402491755158" ID="324soulm4q0373jeojg9qji9fs" MODIFIED="1402491755158" TEXT="&quot;Stream&quot;"/></node></node><node CREATED="1402491755158" FOLDER="true" ID="749n2knt4gf2b5qc7op2b1nbbs" MODIFIED="1402491755158" TEXT="Map Interaction"><node CREATED="1402491755158" ID="6uhmn651cna77edgk5i2n5goua" MODIFIED="1402491755158" TEXT="Map manipulation"/><node CREATED="1402491755158" ID="4jg790dbicj0ola7c80o5om7v2" MODIFIED="1402491755158" TEXT="Venue Selection"/></node><node CREATED="1402491755158" ID="7s04p644gu59a146m5k54kt3am" MODIFIED="1402491755158" TEXT="Physical Buttons (and non app controls)"/></node><node CREATED="1402491755158" FOLDER="true" ID="620hln5bn5uu510s8hrrllgb1a" MODIFIED="1402491755158" TEXT="Location"><node CREATED="1402491755159" ID="5ctm0es0vfes6n173j5pec3r0l" MODIFIED="1402491755159" TEXT="Permission"/><node CREATED="1402491755159" ID="6se32t36hdj6a2r14g19ki4ncp" MODIFIED="1402491755159" TEXT="Non Permission "/><node CREATED="1402491755159" ID="106kcm6l8sg0vjn8b2jt9cak1l" MODIFIED="1402491755159" TEXT="TimeZones"/></node></node><node CREATED="1402491755159" ID="118plru4m5hgdq2f0otkbtac3s" MODIFIED="1402491755159" POSITION="right" TEXT="Security"><node CREATED="1402491755159" ID="7899i1b5lap8r2m68lu3aoohqv" MODIFIED="1402491755159" TEXT="anomynity"/></node><node CREATED="1402491755159" ID="368be7uj0g8l8384h10trmeppm" MODIFIED="1402491755159" POSITION="right" TEXT="Integration"/><node CREATED="1402491755159" ID="6b77jbjq60j71ujqtv3rab4kl1" MODIFIED="1402491755159" POSITION="left" TEXT="Sprints"><node CREATED="1402491755159" ID="0f20husd14a55frct831q4efj9" MODIFIED="1402491755159" TEXT="Charter 1 (Offers/ New, )"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verifying new offers being created and added
+
+User Stories Closed: 
+
+Time: 25 minutes
+
+Platforms: IOS 7.0
+
+Start: 6/11/2014 @ ??? AM
+
+Notes: Checked that an offer can be made and created
+
+
+</text></hook></node><node CREATED="1402491755159" ID="1lorm9sbe8oek68pra7j5490vs" MODIFIED="1402491755159" TEXT="Charter 2 ( Offers / Listing)"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verifying new offers are listed
+
+User Stories Closed: [kok- 283], [kok - 282]
+
+Time: 25 minutes
+
+Platforms: IOS 7.0
+
+Start: 6/11/2014 @ ??? AM
+
+Notes: Checked that all offers added are sorted correctly and can be deleted ( Admin Only).
+Naturally expiring offers should leave the list
+</text></hook></node><node CREATED="1402491755159" ID="76blr3f0midkigii9qg8r2b2pm" MODIFIED="1402491755159" TEXT="Charter 3 (Offers / Edit) ?????"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verifying that added offers can be edited
+
+User Stories Closed: 
+
+Time: 25 minutes
+
+Platforms: IOS 7.0
+
+Start: 6/11/2014 @ ??? AM
+
+Notes: Checked that an offer once made can be edited 
+1: make an offer , save it
+2: Edit the offer
+</text></hook></node><node CREATED="1402491755159" ID="4v6pm1qbakm7305pf7c5m3tbsa" MODIFIED="1402491755159" TEXT="Charter 5 (Offers / Menu Page)"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verifying the Offer page on the main menu is showing local offers
+
+User Stories Closed: 
+
+Time: 25 minutes
+
+Platforms: IOS 7.0
+
+Start: 6/11/2014 @ ??? AM
+
+Notes: Check that Offers created are shown on the main menu in the menu section.
+( While testing I discovered that recent added offers did not show up on the list, will check with tech lead on this page's functionality, also discovered that the "back" button is missing just like in the history page, yet im still able to leave the page due to the menu still being available ( which is isn't for the history page))
+</text></hook></node></node><node CREATED="1402491755159" ID="7llbtuc6ijjl9bh6l079hjp7sj" MODIFIED="1402491755159" POSITION="left" TEXT="Performance"><node CREATED="1402491755159" ID="38brp852ea1md9m6s62p0h8p6t" MODIFIED="1402491755159" TEXT="Battery Sustainability"/></node><node CREATED="1402491755159" ID="05fvnf3hnefug4l5ji0h8c4c9l" MODIFIED="1402491755159" POSITION="left" TEXT="Load"/></node></map>
